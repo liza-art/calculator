7ff52cb25e0d5694ba09d9c6091b87898483d504
liza-art <lizagvozd70@gmail.com>
2024-07-23 17:10:12 +0300

    made a function for applying previous one, and variables

--- file.js
+++ file.js
@@ -20,3 +20,28 @@ function divideNumbers(a,b){
 function subtractNumbers(a,b){
     return a - b;
 }
+
+//create a variable for first number, operator, 
+//and  second number
+
+let firstNum = '';
+let operator = '';
+let secondNum = '';
+
+
+//new function that takes an operator and 2 numbers and calls one of the above functions
+
+function operate(operator, a,b){
+  switch(operator){
+    case '+':
+        return sumNumbers(a,b);
+    case '-':
+        return subtractNumbers(a,b);
+    case '*':
+        return multiplyNumbers(a,b);
+    case '/':
+        return divideNumbers(a,b);
+    default:
+            return 'Invalid operator';
+  }
+}
